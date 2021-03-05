0b3a79abd582c836f507102c46dcec29bbf26e8b
whn <me@whn.se>
2021-03-05 22:02:35 +0100

    add proper binary for rust template

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ set -e
 cd "$(dirname "$0")"
 
 cargo init
+echo -e "[[bin]]\nname =\"app\"\npath = \"src/main.rs\"" >> Cargo.toml
 git rm README.md setup.sh
 git add -A
 git commit -m "Bootstraped using setup.sh"
